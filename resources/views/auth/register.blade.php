644698951beb91f54dbb57d935a226912a95e0a7
石崎貴幸 <>
2025-02-16 17:28:14 +0900

    2/16

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,21 +4,23 @@
 
 <h2>新規ユーザー登録</h2>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+  <div class="form-group">
+    {{ Form::label('ユーザー名') }}
+    {{ Form::text('username',null,['class' => 'input']) }}
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('email',null,['class' => 'input']) }}
+    {{ Form::label('メールアドレス') }}
+    {{ Form::email('email',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+    {{ Form::label('パスワード') }}
+    {{ Form::text('password',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
+    {{ Form::label('パスワード確認') }}
+    {{ Form::text('password_confirmation',null,['class' => 'input']) }}
 
-{{ Form::submit('新規登録') }}
+    {{ Form::submit('新規登録') }}
+  </div>
 
-<p><a href="login">ログイン画面へ戻る</a></p>
+  <p><a href="login">ログイン画面へ戻る</a></p>
 
 {!! Form::close() !!}
 
